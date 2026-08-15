7d8005fdfbcc165bdf957a34df2658863c433875
JeanPiere-start <augustojeampiere15@gmail.com>
2026-08-15 18:24:19 -0500

    Update download filename format for audio files

--- public/js/admin.js
+++ public/js/admin.js
@@ -557,7 +557,7 @@ const adminApp = (() => {
                             <button class="boton boton-secundario" onclick="adminApp.validarGrabacion('${id}', null)" title="Desmarcar / Dejar sin revisar">
                                 ? Sin Revisar
                             </button>
-                            <a class="boton boton-secundario" href="${grab.url_audio}" download="${grab.alias}_${grab.comando}.wav" title="Descargar archivo WAV">
+                            <a class="boton boton-secundario" href="${grab.url_audio}" download="${grab.nombre_archivo || (grab.alias + '_00_' + grab.comando + '.wav')}" title="Descargar archivo WAV">
                                 <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" x2="12" y1="15" y2="3"></line></svg>
                                 WAV
                             </a>
